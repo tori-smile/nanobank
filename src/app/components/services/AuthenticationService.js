c357da50a4a8a5ef657dd95ce235ad4e10946f2f
Viktoryia <i.tori.smile@gmail.com>
2017-12-21 18:01:53 +0300

    added credentials related to session

--- src/app/components/services/AuthenticationService.js
+++ src/app/components/services/AuthenticationService.js
@@ -16,47 +16,36 @@
         return service;
 
         function Login(username, password, callback) {
-
-            /* Dummy authentication for testing, uses $timeout to simulate api call
-             ----------------------------------------------*/
-            $timeout(function () {
-                var response;
-                UserService.GetByUsername(username)
-                    .then(function (user) {
-                        if (user !== null && user.password === password) {
-                            response = { success: true };
-                        } else {
-                            response = { success: false, message: 'Username or password is incorrect' };
-                        }
-                        callback(response);
-                    });
-            }, 1000);
-
-            /* Use this for real authentication
-             ----------------------------------------------*/
-            //$http.post('/api/authenticate', { username: username, password: password })
-            //    .success(function (response) {
-            //        callback(response);
-            //    });
-
+            return UserService.Login(username, password)
+               .then(function (response) {
+                 if (angular.isUndefined(response.success)){
+                   console.log('AuthenticationService Login success', response);
+                   response.success = true;
+                 }else {
+                   console.log('AuthenticationService Login error', response);
+                   response.success = false;
+                 }
+                 callback(response);
+               });
         }
 
-        function SetCredentials(username, password) {
-            var authdata = Base64.encode(username + ':' + password);
+        function SetCredentials(username, password, response) {
+            var authdata = response.access_token;
+            console.log();
 
             $rootScope.globals = {
                 currentUser: {
                     username: username,
-                    authdata: authdata
+                    authdata: authdata,
                 }
             };
 
             // set default auth header for http requests
-            $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata;
+            $http.defaults.headers.common['Authorization'] = response.token_type+ " " + authdata;
 
             // store user details in globals cookie that keeps user logged in for 1 week (or until they logout)
             var cookieExp = new Date();
-            cookieExp.setDate(cookieExp.getDate() + 7);
+            cookieExp.setDate(cookieExp.getDate() + response.expires_in);
             $cookies.putObject('globals', $rootScope.globals, { expires: cookieExp });
         }
 
