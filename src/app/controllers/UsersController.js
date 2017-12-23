79d9565bdb1f916cc6ec51bb0dd8c87e740c5944
Viktoryia <i.tori.smile@gmail.com>
2017-12-23 03:23:25 +0300

    added user dialog call from users page

--- src/app/controllers/UsersController.js
+++ src/app/controllers/UsersController.js
@@ -2,22 +2,46 @@
 
   angular
     .module('app')
-    .controller('UsersController', ['$scope', 'UserService',
+    .controller('UsersController', ['$scope', 'UserService', '$mdDialog', 'helpService',
       UsersController
     ]);
 
-  function UsersController($scope, UserService) {
+  function UsersController($scope, UserService, $mdDialog, helpService) {
     var vm = this;
     vm.activated = false;
+    vm.tableData = [];
     UserService.GetAll().then(function(resourse){
-      vm.tableData = resourse;
-      console.log('table data', vm.tableData);
+      resourse.forEach(function(row){
+        row.phoneNumber = helpService.formatPhoneNumber(row.phoneNumber);
+        vm.tableData.push(row);
+      });
       vm.activated = true;
     });
     $scope.customFilter = customFilter;
     vm.search = "";
     vm.showApproved = true;
     vm.showUnapproved = true;
+
+    $scope.showAdvanced = function(ev, data) {
+       $mdDialog.show({
+         locals:{user: data},
+         controller: 'UserDialogController',
+         templateUrl: 'app/views/partials/userDialog.html',
+         parent: angular.element(document.body),
+         controllerAs: 'vm',
+         theme:"custom",
+         targetEvent: ev,
+         clickOutsideToClose:true,
+         fullscreen: false
+       })
+       .then(function(answer) {
+         $scope.status = 'You said the information was "' + answer + '".';
+         console.log($scope.status);
+       }, function() {
+         $scope.status = 'You cancelled the dialog.';
+         console.log($scope.status);
+       });
+     };
 
     function customFilter(user){
       return vm.showApproved && user.isApproved || vm.showUnapproved && !user.isApproved;
