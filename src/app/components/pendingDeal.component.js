860073c96c08928b141440a0fd4e9081e06ca7bb
Viktoryia <i.tori.smile@gmail.com>
2017-12-29 17:49:03 +0300

    added pending deal card on dashboard

--- src/app/components/pendingDeal.component.js
+++ src/app/components/pendingDeal.component.js
@@ -12,13 +12,40 @@
           }
         });
 
-    function pendingDealComponent(){
+    pendingDealComponent.$inject = ['$state','UserService', 'DealService', 'helpService','$mdDialog'];
+    function pendingDealComponent($state, UserService, DealService, helpService, $mdDialog){
       var vm = this;
-      vm.hello= "hello";
+      vm.dataLoading=false;
 
       vm.$onInit = function(){
-          console.log("im here", vm);
+        vm.showButton = vm.deal.ownerUserName !== UserService.currentUsername;
       }
+
+      vm.showConfirm = function(forDeletion) {
+        var title = forDeletion ? 'Вы действительно хотите удалить данную заявку?':'Вы действительно хотите откликнуться на данную заявку?';
+        var textContent = forDeletion ? 'После подтверждения удаления данная заявка окажется недоступной для Вас и других пользователей.' : 'Подтверждение действия означает согласие на условия заёмщика.';
+        var ok = forDeletion ? 'удалить' : 'откликнуться';
+
+        $mdDialog.show(helpService.getConfirmDialog(title, textContent, ok)).then(function() {
+          makeDealAction(forDeletion).then(function(response){
+            $state.reload();
+            vm.dataLoading = false;
+          });
+        }, function() {
+          // console.log('You decided to keep your debt.');
+          // console.log(vm.user);
+        });
+
+        function makeDealAction(deletion){
+          if (deletion){
+            vm.dataLoading=true;
+            return DealService.Delete(vm.deal.id);
+          }else {
+            console.log(vm.deal.id, UserService.currentUsername);
+            return DealService.Response(vm.deal.id, UserService.currentUsername);
+          }
+        }
+      };
 
     }
 })();
