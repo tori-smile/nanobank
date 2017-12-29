545f48e793493838823fdb6dc36876bb4d41228c
Viktoryia <i.tori.smile@gmail.com>
2017-12-29 18:06:06 +0300

    update due to transferring dialog logic to helpService

--- src/app/controllers/UserDialogController.js
+++ src/app/controllers/UserDialogController.js
@@ -32,7 +32,11 @@
     };
 
     $scope.showConfirm = function(ev, forDeletion) {
-      $mdDialog.show(getConfirmDialog(forDeletion)).then(function() {
+      var title = forDeletion ? 'Вы действительно хотите удалить аккаунт данного пользователя?':'Вы действительно хотите утвердить аккаунт данного пользователя?'
+      var textContent = forDeletion ? 'После подтверждения удаления учётная запись пользователя сотрётся из базы данных.' : 'После подтверждения действия пользователю предоставится доступ к ресурсам приложения.'
+      var ok = forDeletion ? 'удалить.' : 'подтвердить.'
+      
+      $mdDialog.show(helpService.getConfirmDialog(title, textContent, ok)).then(function() {
         makeUserAction(forDeletion).then(function(response){
           $state.reload();
         });
@@ -41,30 +45,6 @@
         // console.log(vm.user);
       });
 
-      function getConfirmDialog(forDeletion){
-        var title = forDeletion ? 'Вы действительно хотите удалить аккаунт данного пользователя?':'Вы действительно хотите утвердить аккаунт данного пользователя?'
-        var textContent = forDeletion ? 'После подтверждения удаления учётная запись пользователя сотрётся из базы данных.' : 'После подтверждения действия пользователю предоставится доступ к ресурсам приложения.'
-        var ok = forDeletion ? 'удалить.' : 'подтвердить.'
-
-        var confirm = $mdDialog.confirm({
-                      onComplete: function afterShowAnimation() {
-                          var $dialog = angular.element(document.querySelector('md-dialog'));
-                          var $actionsSection = $dialog.find('md-dialog-actions');
-                          var $cancelButton = $actionsSection.children()[0];
-                          var $confirmButton = $actionsSection.children()[1];
-                          angular.element($confirmButton).addClass('md-raised md-accent');
-                          angular.element($cancelButton).addClass('md-accent');
-                      }
-        })
-              .title(title)
-              .textContent(textContent)
-              .ariaLabel('Lucky day')
-              .targetEvent(ev)
-              .ok('Да, ' + ok)
-              .cancel('Отмена');
-
-        return confirm;
-      }
 
       function makeUserAction(deletion){
         if (deletion){
