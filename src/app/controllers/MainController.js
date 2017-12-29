4f1e87d7ad3fb3033418ea6e8ed697025bad7446
Viktoryia <i.tori.smile@gmail.com>
2017-12-29 18:01:03 +0300

    removed unused logic in main controller

--- src/app/controllers/MainController.js
+++ src/app/controllers/MainController.js
@@ -13,23 +13,17 @@
     vm.menuItems = [ ];
     vm.selectItem = selectItem;
     vm.toggleItemsList = toggleItemsList;
-    //vm.showActions = showActions;  unused
     UserService.setCurrentUser(UserService.currentUsername).then(function(){
       vm.userFullName = UserService.currentUser.firstName +" "+ UserService.currentUser.lastName;
     });
     vm.title = $state.current.data.title;
     vm.showSimpleToast = showSimpleToast;
-    vm.toggleRightSidebar = toggleRightSidebar;
 
     navService
       .loadAllItems()
       .then(function(menuItems) {
         vm.menuItems = [].concat(menuItems);
       });
-
-    function toggleRightSidebar() {
-        $mdSidenav('right').toggle();
-    }
 
     function toggleItemsList() {
       var pending = $mdBottomSheet.hide() || $q.when(true);
@@ -43,32 +37,6 @@
       vm.title = item.name;
       vm.toggleItemsList();
       vm.showSimpleToast(vm.title);
-    }
-
-    function showActions($event) {
-        $mdBottomSheet.show({
-          parent: angular.element(document.getElementById('content')),
-          templateUrl: 'app/views/partials/bottomSheet.html',
-          controller: [ '$mdBottomSheet', SheetController],
-          controllerAs: "vm",
-          bindToController : true,
-          targetEvent: $event
-        }).then(function(clickedItem) {
-          clickedItem && $log.debug( clickedItem.name + ' clicked!');
-        });
-
-        function SheetController( $mdBottomSheet ) {
-          var vm = this;
-
-          vm.actions = [
-            { name: 'Share', icon: 'share', url: 'https://twitter.com/intent/tweet?text=Angular%20Material%20Dashboard%20https://github.com/flatlogic/angular-material-dashboard%20via%20@flatlogicinc' },
-            { name: 'Star', icon: 'star', url: 'https://github.com/flatlogic/angular-material-dashboard/stargazers' }
-          ];
-
-          vm.performAction = function(action) {
-            $mdBottomSheet.hide(action);
-          };
-        }
     }
 
     function showSimpleToast(title) {
