aad64be78f7c33dd571301fb3db3464e4c368af5
Viktoryia <i.tori.smile@gmail.com>
2017-12-29 18:16:02 +0300

    service for deal operations

---
+++ src/app/components/services/DealService.js
@@ -0,0 +1,70 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('DealService', DealService);
+
+    DealService.$inject = ['$http', '$httpParamSerializerJQLike', '$state'];
+    function DealService($http, $httpParamSerializerJQLike, $state) {
+        var service = {};
+        var API_BASE = 'http://nanobank.azurewebsites.net'
+        var specialContentType = {headers: {'Content-Type': 'application/x-www-form-urlencoded'}};
+
+        service.GetAll = GetAll;
+        service.GetAllOpen = GetAllOpen;
+        service.GetAllUserDeals = GetAllUserDeals;
+        service.Create = Create;
+        service.Update = Update;
+        service.Response = Response;
+        service.Delete = Delete;
+        return service;
+
+        function GetAll() {
+            return $http.get(API_BASE + '/api/deal/all').then(handleSuccess, handleError('Error getting all deals'));
+        }
+
+        function GetAllOpen() {
+            return $http.get(API_BASE + '/api/deal/all/opened').then(handleSuccess, handleError('Error getting all opened deals'));
+        }
+
+        function GetAllUserDeals(username) {
+            return $http.get(API_BASE + '/api/deal/' + username + '/all').then(handleSuccess, handleError('Error getting all deals'));
+        }
+
+        function Create(deal) {
+          return $http.post(API_BASE + '/api/deal/register', processData(deal), specialContentType).then(handleSuccess, handleError('Error creating deal'));
+        }
+
+        function Update(deal) {
+          return $http.put(API_BASE + '/api/deal/' + deal.id, deal).then(handleSuccess, handleError('Error updating deal'));
+        }
+
+        function Response(dealId, username) {
+          console.log(username);
+            return $http.put(API_BASE + '/api/deal/respond/' + dealId, JSON.stringify(username)).then(handleSuccess, handleError('Error resposing deal'));
+        }
+
+        function Delete(dealid) {
+          // application/x-www-form-urlencoded
+            return $http.delete(API_BASE + '/api/deal/' + dealid, '', specialContentType).then(handleSuccess, handleError('Error deleting deal'));
+        }
+        // private functions
+
+        function processData(data){
+          return $httpParamSerializerJQLike(data);
+        }
+
+        function handleSuccess(res) {
+            console.log(res.data);
+            return res.data;
+        }
+
+        function handleError(error) {
+            return function () {
+                return { success: false, message: error };
+            };
+        }
+    }
+
+})();
