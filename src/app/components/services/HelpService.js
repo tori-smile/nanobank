205b108946867f3703d7c7deb7c0732ece0d5343
Viktoryia <i.tori.smile@gmail.com>
2017-12-29 17:35:38 +0300

    added format date period number of monthes -> word representation

--- src/app/components/services/HelpService.js
+++ src/app/components/services/HelpService.js
@@ -11,6 +11,7 @@
 
     service.formatPhoneNumber = formatPhoneNumber;
     service.formatRating = formatRating;
+    service.formatDatePeriod = formatDatePeriod;
 
     function formatPhoneNumber(phoneNumber){
       var match = /^(\+?375|80)?(29|25|44|33)(\d{3})(\d{2})(\d{2})$/.exec(phoneNumber);
@@ -28,6 +29,24 @@
         match = [0,0];
       }
       return sign + match[1];
+    }
+
+    function formatDatePeriod(monthesCount){
+      var answer = '';
+      var monthes = [1, 6, 12, 18];
+      var inWords = ['месяц','полгода', 'год', 'полтора года']
+
+      if (monthes.indexOf(monthesCount) != -1){
+        answer = inWords[monthes.indexOf(monthesCount)];
+      }else if (monthesCount % 10 == 1) {
+        answer = monthesCount + ' месяц';
+      }else if ([2,3,4].indexOf(monthesCount % 10)!=-1 && (monthesCount<10 || monthesCount>20)) {
+        answer = monthesCount + ' месяца';
+      }else {
+        answer = monthesCount + ' месяцев';
+      }
+
+      return answer;
     }
 
     return service;
