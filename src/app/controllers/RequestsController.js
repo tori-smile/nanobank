884cdb04b5748e34b22a01647fed1953ec176923
Viktoryia <i.tori.smile@gmail.com>
2017-12-31 04:15:50 +0300

    made deals table unclickable

--- src/app/controllers/RequestsController.js
+++ src/app/controllers/RequestsController.js
@@ -13,7 +13,6 @@
     DealService.GetAll().then(function(resourse){
       vm.tableData = resourse;
       vm.activated = true;
-      console.log(vm.tableData);
     });
     $scope.customFilter = customFilter;
     vm.search = "";
@@ -22,30 +21,8 @@
     vm.showUnassigned = true;
     vm.showClosed = true;
 
-    $scope.showAdvanced = function(ev, data) {
-       $mdDialog.show({
-         locals:{user: data},
-         controller: 'UserDialogController',
-         templateUrl: 'app/views/partials/userDialog.html',
-         parent: angular.element(document.body),
-         controllerAs: 'vm',
-         theme:"custom",
-         targetEvent: ev,
-         clickOutsideToClose:true,
-         fullscreen: false
-       })
-       .then(function(answer) {
-         $scope.status = 'You said the information was "' + answer + '".';
-         console.log($scope.status);
-       }, function() {
-         $scope.status = 'You cancelled the dialog.';
-         console.log($scope.status);
-       });
-     };
-
     function customFilter(deal){
       var assigned = angular.isDefined(deal.creditorUserName) && deal.creditorUserName !== null;
-      console.log(deal, assigned);
       var ok = vm.showOpened && !deal.isClosed || vm.showClosed && deal.isClosed;
 
       return vm.showUnassigned && !assigned ||
