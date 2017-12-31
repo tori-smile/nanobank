3f4bc62da80c46a138c6a8b221306401a001795b
Viktoryia <i.tori.smile@gmail.com>
2017-12-31 06:29:05 +0300

    default rating on profile page

--- src/app/controllers/ProfileController.js
+++ src/app/controllers/ProfileController.js
@@ -16,6 +16,8 @@
       $timeout(function() {
         vm.user = UserService.currentUser;
         vm.user.phoneNumber = helpService.formatPhoneNumber(vm.user.phoneNumber)
+        vm.user.ratingNegative = vm.user.ratingNegative ? vm.user.ratingNegative: 0;
+        vm.user.ratingPositive = vm.user.ratingPositive ? vm.user.ratingPositive: 0;
         if (angular.isUndefined(vm.user.firstName)){
           waitForCurrentUser()
         };
